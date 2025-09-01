b0cab05aa5e980f20cacad173e9172453e27e76b
Kajal Saini <sizukabts91@gmail.com>
2025-09-01 15:41:38 +0530

    Create about section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import { Todo } from './Components/Todo';
+import { AddTodo } from './Components/AddTodo';
+import { Footer } from './Components/Footer';
+import { About } from "./Components/About";
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  const onDelete = (todo) => {
+    console.log("i'm ondelete of todo", todo);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  const addTodo = (title, desc) => {
+    console.log("i'm adding this todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+
+  }
+
+  const [todos, setTodos] = useState([initTodo]);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header title="My Todo's List" searchBar={false} />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <AddTodo addTodo={addTodo} />
+              <Todo todos={todos} onDelete={onDelete} />
+            </>
+          }
+          />
+
+          <Route path="/about" element={<About />} />
+
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
-
 export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni modi optio explicabo exercitationem facilis quasi libero ullam ipsum a fugiat deleniti voluptatibus minus nesciunt aliquam, ab sapiente molestiae placeat, quas quisquam ut provident consequuntur.</p>
+    </div>
+  )
+}
